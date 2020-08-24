7f3ce8fb82fd702208dc7e44497157e36f672ab0
Jared Hubbard <hubbard.abstract@gmail.com>
2020-08-24 10:20:40 -0700

    Handling Assignment
    Got very confused in the wording of the assignment and made it wayyy more complicated than it needed to be. But after a long struggle and decyphering what the assignment wanted i was able to get it

---
+++ HandlingAssignment1/HandlingAssignment1/Button.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HandlingAssignment1
+{
+    public class MathOps
+    {
+        public int AddFour(int num)
+        {
+            return num + 4;
+        }
+
+        public int SubTen(int num)
+        {
+            return num - 10; 
+        }
+
+        public int MultiNine(int num)
+        {
+            return num * 9;
+        }
+    }
+}
---
+++ HandlingAssignment1/HandlingAssignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using HandlingAssignment1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do a math operation on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            var Ops = new MathOps();
+            Console.WriteLine();
+            Console.WriteLine(Ops.AddFour(userInput));
+            Console.WriteLine();
+            Console.WriteLine(Ops.SubTen(userInput));
+            Console.WriteLine();
+            Console.WriteLine(Ops.MultiNine(userInput));
+            Console.ReadLine();
+        }
+    }
+}
