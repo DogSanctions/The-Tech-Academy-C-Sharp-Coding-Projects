f944ff5dbf3bd4ffdab9dc3508256e94fb925c41
Jared Hubbard <hubbard.abstract@gmail.com>
2020-08-27 11:39:37 -0700

    Assignment 164

---
+++ Assignment164/Assignment164/MathOps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Operations
+{
+    public class MathOps
+    {
+        public int AddTen(int num)
+        {
+            return num + 10;
+        }
+
+        public int SubTwo(int num)
+        {
+            return num - 2;
+        }
+
+        public int MultiEight(int num)
+        {
+            return num * 8;
+        }
+
+        public int DivideTwelve(int num)
+        {
+            return num / 12;
+        }
+
+        public double dAddFive(double num)
+        {
+            return num + 5.5F;
+        }
+
+        public double dSubFour(double num)
+        {
+            return num + 4.3;
+        }
+
+        public double dModularSixteen(double num)
+        {
+            return num % 16.78;
+        }
+
+        public int sMultiNine(int num)
+        {
+            return num * 9;
+        }
+    }
+}
---
+++ Assignment164/Assignment164/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Operations;
+
+namespace Assignment164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a integer you would like to see some math operations done on.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            var Ops = new MathOps();
+            Console.WriteLine();
+            Console.WriteLine(Ops.AddTen(userInput));
+            Console.WriteLine();
+            Console.WriteLine(Ops.SubTwo(userInput));
+            Console.WriteLine();
+            Console.WriteLine(Ops.MultiEight(userInput));
+            Console.WriteLine();
+            Console.WriteLine(Ops.DivideTwelve(userInput));
+            Console.WriteLine();
+            Console.WriteLine("Please enter another number to do some operations on, but this time please make it a decimal");
+            double userInput2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(Ops.dAddFive(userInput2));
+            Console.WriteLine();
+            Console.WriteLine(Ops.dSubFour(userInput2));
+            Console.WriteLine();
+            Console.WriteLine(Ops.dModularSixteen(userInput2));
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number");
+            string userInput3 = Convert.ToString(Console.ReadLine());
+            try
+            {
+                int strVal = Int32.Parse(userInput3);
+                Console.WriteLine(Ops.sMultiNine(strVal));
+            } 
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
