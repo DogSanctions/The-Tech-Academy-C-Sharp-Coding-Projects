f2a40bda02d9324c6e931321f359518ede18534f
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-01 12:08:25 -0700

    Assignment 221
    Takes in two parameters into a void method, and does a math operation on the first integer and displays the second one

---
+++ Assignment221/Assignment221/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            VoidMethod math = new VoidMethod();
+            math.SecretMethod(userInput, userInput2);
+        } 
+    }
+}
---
+++ Assignment221/Assignment221/VoidMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment221
+{
+    public class VoidMethod
+    {
+        public void SecretMethod(int num1, int num2)
+        {
+            int result = num1 + 10;
+            Console.WriteLine(num2);
+        }
+    }
+}
