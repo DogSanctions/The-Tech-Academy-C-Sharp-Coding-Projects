c3b7ea47b55946f79543df653ec479917a434304
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-02 11:56:54 -0700

    Assignment 224

---
+++ Assignment 224/Assignment 224/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_224
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            VoidMethod math = new VoidMethod();
+            math.DivideMethod(userInput);
+            Console.WriteLine("Please enter a number");
+            float userInput2 = Convert.ToInt32(Console.ReadLine());
+            math.DivideMethod(userInput2);
+            Console.WriteLine("Please enter a number");
+            int userInput3 = Convert.ToInt32(Console.ReadLine());
+            math.OutputPara(userInput3);
+        }
+    }
+}
---
+++ Assignment 224/Assignment 224/VoidMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Assignment_224
+{
+    public class VoidMethod
+    {
+        public void DivideMethod(int num) 
+        {
+            int result = num / 2;
+            Console.WriteLine(result);
+        }
+        public void DivideMethod(float num1)
+        {
+            float result1 = num1 * 3;
+            Console.WriteLine(result1);
+        }
+
+        public void OutputPara(int num2)
+        {
+            int intialParameter = num2;
+            OutIntialParameter(out intialParameter);
+            Console.WriteLine(intialParameter);
+        }
+        static void OutIntialParameter(out int num2)
+        {
+            num2 = 44;
+        }
+    }
+}
