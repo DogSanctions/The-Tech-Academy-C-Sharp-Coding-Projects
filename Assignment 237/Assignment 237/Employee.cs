040f152a8665db754949fce0ca436a6d0fd82f53
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-05 12:53:40 -0700

    Assignment 237

---
+++ Assignment 237/Assignment 237/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_237
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Assignment 237/Assignment 237/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment_237
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person obj = new Employee()
+            {
+                firstName = "Sample",
+                lastName = "Student",
+            };
+            obj.SayName();
+        }
+    }
+}
