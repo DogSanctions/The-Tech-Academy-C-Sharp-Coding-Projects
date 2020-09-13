8df4d502f48d09665027ce78df00a3ef14301176
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-13 08:17:56 -0700

    Assignment 254

---
+++ Assignment 254/Assignment 254/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_254
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week, please.");
+            try
+            {
+                Days userInput = (Days)Enum.Parse(typeof(Days), Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment 254/Assignment 254/Week.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_254
+{
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
