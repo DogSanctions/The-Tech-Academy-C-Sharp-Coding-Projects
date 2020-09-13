4db5d2580e680e30c4c948877aa19db6fa50d7a4
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-13 07:57:29 -0700

    Assignment 248
    Generic Type

---
+++ Assignment 248/Assignment 248/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_248
+{
+    public class Employee<T>
+    {
+        public void Add(T input) { }
+        public List<T> things { get; set; }
+    }   
+        
+}
---
+++ Assignment 248/Assignment 248/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment_248
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            Employee<int> employee2 = new Employee<int>();
+            employee.things = new List<string>() { "Abby", "Blake", "Georgie"};
+            employee2.things = new List<int>() { 3, 33, 333 };
+
+            foreach (var thing in employee.things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach(var thing1 in employee2.things)
+            {
+                Console.WriteLine(thing1);
+            }
+
+        }
+    }
+}
