11501c20bd1b5c07c48a9d0b1008e514ee409630
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-13 08:31:11 -0700

    Assignment 260

---
+++ Assignment 260/Assignment 260/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assignment_260
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 200;
+            Console.WriteLine(number.Amount);
+        }
+        struct Number
+        {
+            public double Amount;
+        }
+    }
+}
