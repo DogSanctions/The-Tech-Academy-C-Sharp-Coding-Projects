ce66317fc72cd5ac0f5769f61810c8e0caefbb4d
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-21 13:09:49 -0700

    fixed

---
+++ TwentyOne/ClassesAndObjects/Dealer (2).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TwentyOne;
+
+namespace TwentyOne
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Jared\source\repos\ClassesAndObjects\logs.txt", true))
+            {
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
---
+++ TwentyOne/ClassesAndObjects/IWalkAway (2).cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwentyOne;
+
+namespace TwentyOne
+{
+    interface IWalkAway
+    {
+        void WalkAway(Player player);
+    }
+}
