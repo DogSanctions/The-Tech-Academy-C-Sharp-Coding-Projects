6443ebd535801af5b58e28df320e460f0c89cb3b
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-21 15:30:29 -0700

    Assignment 290
    1. Asks the user for a number.
    
    2. Logs that number to a text file.
    
    3. Prints the text file back to the user.

---
+++ Assignment 290/Assignment 290/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment_290
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userInput = Convert.ToString(Console.ReadLine());
+            File.WriteAllText("C:\\Users\\Jared\\source\\repos\\Assignment 290\\logs.txt", userInput);
+            using (StreamReader file = new StreamReader("C:\\Users\\Jared\\source\\repos\\Assignment 290\\logs.txt", true))
+            {
+                Console.WriteLine(file.ReadToEnd());
+            }
+        }
+    }
+}
