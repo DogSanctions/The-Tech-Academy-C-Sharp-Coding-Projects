7e8d18f7faed10ecfe41c7b18f03662e9aace4cf
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-21 18:26:38 -0700

    Assignment 293
    1. Prints the current date and time to the console.
    
    2. Asks the user for a number.
    
    3. Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.

---
+++ Assignment293/Assignment293/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime todaysTime = DateTime.Now;
+            DateTime usersChange = todaysTime.AddTicks(userInput);
+            Console.WriteLine(usersChange);
+            Console.ReadLine();
+            
+        }
+    }
+}
