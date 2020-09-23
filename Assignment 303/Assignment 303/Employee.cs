132ad8b94570925b311dd43b2a7a1a18003825df
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-23 03:59:50 -0700

    Assignment 303
    1. Create a const variable.
    
    2. Create a variable using the keyword "var.”
    
    3. Chain two constructors together.

---
+++ Assignment 303/Assignment 303/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_303
+{
+    public class Employee
+    {
+        public string Name;
+        public string Address;
+        public int Salary;
+
+        public Employee(string name) : this(name, string.Empty)
+        {
+        }
+        public Employee(string name, string address) : this(name, address, 40000)
+        {
+        }
+
+        public Employee(string name, string address, int salary)
+        {
+            this.Name = name;
+            this.Address = address;
+            this.Salary = salary;
+        }
+    }
+}
---
+++ Assignment 303/Assignment 303/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string welcomeSaying = "Welcome to the employee sign up system";
+            Console.WriteLine("Hi, {0}", welcomeSaying);
+            Console.WriteLine("Please enter your name, address, and your salary. If you haven't been given a different salary, you will be starting at the base salary of 40,000$");
+            var userInput = Convert.ToString(Console.ReadLine());
+
+            var employee = new Employee(userInput);
+          
+        }
+    }
+}
