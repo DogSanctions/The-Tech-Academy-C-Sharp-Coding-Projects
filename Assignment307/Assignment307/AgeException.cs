bc9f74b65f663fdac074c4b43fa2907e819f9444
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-24 13:39:22 -0700

    Assignment 307
    1. Ask the user for their age.
    
    2. Display the year the user was born.
    
    3. Exceptions must be handled using “try/catch.”
    
    4. Display appropriate error messages if the user enters zero or negative numbers.
    
    5. Display a general message if an exception was caused by anything else.

---
+++ Assignment307/Assignment307/AgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment307
+{
+    public class AgeException : Exception
+    {
+    
+    }
+}
+
---
+++ Assignment307/Assignment307/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter how old you are.");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int todaysTime = DateTime.Today.Year;
+                if (userInput <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    var ageFinder = todaysTime - userInput;
+                    Console.WriteLine(ageFinder);
+                    Console.ReadLine();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please only enter numbers when entering your entering your correct age.");
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter the correct numbers for your age.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter in your correct age.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
