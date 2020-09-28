bb30a53e6d9a7fdf26bdfc3119a88a539d7b5a8e
Jared Hubbard <hubbard.abstract@gmail.com>
2020-09-27 22:15:41 -0700

    Assignment 444
    Need help

---
+++ Assignment 444/Assignment 444/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Assignment_444
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new StudentData())
+            {
+                var student = new Student { StudentId = 1, FirstName = "Gerald", LastName = "Howzer", DateOfBirth = DateTime.Now };
+
+                ctx.students.Add(student);
+                ctx.SaveChanges();
+            }
+            
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+}
---
+++ Assignment 444/Assignment 444/StudentData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_444
+{
+    public class StudentData: DbContext
+    {
+        public StudentData() : base()
+        {
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
