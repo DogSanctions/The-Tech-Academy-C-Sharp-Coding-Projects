377dceb2969b50c5625742f56215e3e65ecb48e3
Jared Hubbard <hubbard.abstract@gmail.com>
2020-10-03 20:52:30 -0700

    Contoso University
    MVC 5 Live Project Course Tutorial

---
+++ WebAppTutor/WebAppTutor/DAL/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WebAppTutor.Models;
+
+namespace WebAppTutor.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ WebAppTutor/WebAppTutor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppTutor.MvcApplication" Language="C#" %>
